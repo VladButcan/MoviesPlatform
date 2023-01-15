a522f3537b1e4901c3059433c70ab7c0cbd86b42
VladButcan <butcanvlad10@gmail.com>
2023-01-15 22:50:07 +0200

    Make Users class to implement Notify class and add function setNotify for notify all users that subscribed to added movie

--- src/json/Users/Users.java
+++ src/json/Users/Users.java
@@ -1,8 +1,32 @@
 package json.Users;
+import homepageautentificat.MoviesPage.Movies;
+import homepageautentificat.SeeDetailsPage.Notify;
 import json.Credentials.Credentials;
+import json.Credentials.Notifications;
+import json.Json;
 
-public final class Users {
+import java.util.List;
+
+public final class Users implements Notify {
     private Credentials credentials = new Credentials();
+
+    /**
+     * Function for notify all users
+     * @param notifications notification about delete or add movie
+     */
+    public void setNotify(final Notifications notifications, final Movies movie) {
+        List<Users> usersArrayList = new Json().getUsers();
+        for (Users user: usersArrayList) {
+            if (!movie.getCountriesBanned().contains(user.getCredentials().getCountry())) {
+                for (String genre: movie.getGenres()) {
+                    if (user.getCredentials().getSubscribeGenres().contains(genre)) {
+                        user.getCredentials().setNotifications(notifications);
+                        break;
+                    }
+                }
+            }
+        }
+    }
     public Credentials getCredentials() {
         return credentials;
     }
