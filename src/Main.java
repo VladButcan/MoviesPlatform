493b48e4b27e75a36f89af604d3e0f98605baec0
VladButcan <butcanvlad10@gmail.com>
2023-01-16 00:30:21 +0200

    Change for loop and update them with factory design pattern

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import json.Actions.Actions;
+import json.Actions.*;
 import homepageneautentificat.HomePageNeautentificat;
 import currentpageinterface.CurrentPage;
-import json.Actions.AddedMovie;
-import json.Actions.Back;
-import json.Actions.DeleteMovie;
 import json.Json;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
@@ -21,74 +18,37 @@ public class Main {
     public static void main(final String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         ArrayNode output = objectMapper.createArrayNode();
+
         ArrayList<String> arrayList = new ArrayList<String>();
         arrayList.add("Homepage neautentificat");
-        objectMapper = new ObjectMapper();
+
         Json json = objectMapper.readValue(new File(args[0]), Json.class);
+
         CurrentPage currentPage = new HomePageNeautentificat();
-        AddedMovie addedMovie = new AddedMovie();
-        DeleteMovie deleteMovie = new DeleteMovie();
+
         Back back = new Back();
         back.resetHistoryAccessPage(new ArrayList<CurrentPage>());
         back.setHistoryAccessPage(currentPage);
 
         JsonOut jsonOut = new JsonOut();
+        ActionsType actionsType = new ActionsType();
+        TypeOfActions typeOfActions;
+
         for (Actions actions: json.getActions()) {
-            switch (actions.getType()) {
-                case "change page":
-                    currentPage = actions.changeCurrentPage(currentPage, actions, json.getUsers(),
-                            json.getMovies(), output);
-                    if (!back.getHistoryAccessPage().get(
-                            back.getHistoryAccessPage().size() - 1).
-                            getClass().getName().equals(currentPage.getClass().getName())) {
-                        back.getHistoryAccessPage().add(currentPage);
-                    }
-                    break;
-                case "on page":
-                    currentPage = actions.onPage(currentPage, actions, json.getUsers(),
-                            json.getMovies(), output);
-                    if (!back.getHistoryAccessPage().get(
-                                    back.getHistoryAccessPage().size() - 1).
-                            getClass().getName().equals(currentPage.getClass().getName())) {
-                        back.getHistoryAccessPage().add(currentPage);
-                    }
-                    break;
-                case "database":
-                    switch (actions.getFeature()) {
-                        case "add":
-                            json.setMovies(addedMovie.add(
-                                    json.getUsers(), json.getMovies(), actions, output));
-                            break;
-                        case "delete":
-                            json.setMovies(deleteMovie.delete(
-                                    json.getUsers(), json.getMovies(), actions, output));
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "back":
-                    back.resetHistoryAccessPage(back.act(back.getHistoryAccessPage(), output));
-                    currentPage = back.getHistoryAccessPage().get(
-                            back.getHistoryAccessPage().size() - 1);
-                    actions.goBack(currentPage, json.getMovies(), output);
-                    break;
-                default:
-                    break;
-            }
+            typeOfActions = actionsType.returnType(actions);
+            currentPage = typeOfActions.act(currentPage, json.getUsers(), actions, json.getMovies(), output);
         }
-        jsonOut = new JsonOut();
+        /**
+         * Recommend the best movie for user if they are premium
+         */
         if (!jsonOut.getActiveUser().equals("No user")) {
             if (jsonOut.getCurrentUser().getCredentials().getAccountType().equals("premium")) {
                 Recommendations recommendations = new Recommendations();
                 recommendations.act(json.getMovies(), output);
             }
-        } else {
-
         }
+
         ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
         objectWriter.writeValue(new File(args[1]), output);
-        objectWriter.writeValue(new File("checker/\\"
-                + args[0].substring(args[0].lastIndexOf("\\") + 1)), output);
     }
 }
