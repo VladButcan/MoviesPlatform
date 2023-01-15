6ba2ee7f99e19244661f03f13284189aec3a32e6
VladButcan <butcanvlad10@gmail.com>
2023-01-15 22:48:16 +0200

    Make all list like static for call them in other Class

--- src/json/Json.java
+++ src/json/Json.java
@@ -5,9 +5,9 @@ import homepageautentificat.MoviesPage.Movies;
 import java.util.List;
 
 public final class Json {
-    private List<Users> users;
-    private List<Actions> actions;
-    private List<Movies> movies;
+    private static List<Users> users;
+    private static List<Actions> actions;
+    private static List<Movies> movies;
 
     public List<Users> getUsers() {
         return users;
@@ -21,7 +21,23 @@ public final class Json {
         return movies;
     }
 
+    /**
+     * Set users list
+     * @param users new list of users
+     */
+    public void setUsers(final List<Users> users) {
+        Json.users = users;
+    }
+
+    /**
+     * Set actions list
+     * @param actions new list of users
+     */
+    public void setActions(final List<Actions> actions) {
+        Json.actions = actions;
+    }
+
     public void setMovies(final List<Movies> movies) {
-        this.movies = movies;
+        Json.movies = movies;
     }
 }
