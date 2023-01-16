c36b4fe6267e887b3a5836b0d70dc0e9ecfda005
VladButcan <butcanvlad10@gmail.com>
2023-01-16 02:42:19 +0200

    Add Database class that implements TypeOfActions and is called when actions type is database and return new current page

---
+++ src/json/Actions/Database.java
@@ -0,0 +1,33 @@
+package json.Actions;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import currentpageinterface.CurrentPage;
+import homepageautentificat.MoviesPage.Movies;
+import json.Json;
+import json.Users.Users;
+
+import java.util.List;
+
+public final class Database implements TypeOfActions {
+    @Override
+    public CurrentPage act(final CurrentPage currentPage,
+                    final List<Users> usersList, final Actions actions,
+                    final List<Movies> moviesList, final ArrayNode output) {
+        AddedMovie addedMovie = new AddedMovie();
+        DeleteMovie deleteMovie = new DeleteMovie();
+
+        switch (actions.getFeature()) {
+            case "add":
+                new Json().setMovies(addedMovie
+                        .add(usersList, moviesList, actions, output));
+                break;
+            case "delete":
+                new Json().setMovies(deleteMovie
+                        .delete(usersList, moviesList, actions, output));
+                break;
+            default:
+                break;
+        }
+        return currentPage;
+    }
+}
