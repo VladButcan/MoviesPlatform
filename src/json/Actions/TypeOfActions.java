f2c2bb0e7d89aefb5b1dd239cee23f673695f8ca
VladButcan <butcanvlad10@gmail.com>
2023-01-16 02:46:46 +0200

    Add TypeOfActions interface that call the function act for ations type

---
+++ src/json/Actions/TypeOfActions.java
@@ -0,0 +1,22 @@
+package json.Actions;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import currentpageinterface.CurrentPage;
+import homepageautentificat.MoviesPage.Movies;
+import json.Users.Users;
+
+import java.util.List;
+
+public interface TypeOfActions {
+    /**
+     * Function
+     * @param currentPage variable for save current user page access
+     * @param usersList list of users
+     * @param actions action node from json input
+     * @param moviesList list of movies
+     * @param output array node for output
+     */
+    CurrentPage act(CurrentPage currentPage,
+             List<Users> usersList, Actions actions,
+             List<Movies> moviesList, ArrayNode output);
+}
