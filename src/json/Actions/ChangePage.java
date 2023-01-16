930bcfb175fabf17c111da3c5c3ba3a2c830350b
VladButcan <butcanvlad10@gmail.com>
2023-01-16 02:41:31 +0200

    Add ChangePage class that is called when actions type is change page and return new current page

---
+++ src/json/Actions/ChangePage.java
@@ -0,0 +1,24 @@
+package json.Actions;
+
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import currentpageinterface.CurrentPage;
+import homepageautentificat.MoviesPage.Movies;
+import json.Users.Users;
+
+import java.util.List;
+
+public final class ChangePage implements TypeOfActions {
+    @Override
+    public CurrentPage act(CurrentPage currentPage,
+                    final List<Users> usersList, final Actions actions,
+                    final List<Movies> moviesList, final ArrayNode output) {
+        Back back = new Back();
+        currentPage = actions.changeCurrentPage(currentPage, actions, usersList,
+                moviesList, output);
+        if (!back.getHistoryAccessPage().get(back.getHistoryAccessPage().size() - 1)
+                .getClass().getName().equals(currentPage.getClass().getName())) {
+            back.getHistoryAccessPage().add(currentPage);
+        }
+        return currentPage;
+    }
+}
